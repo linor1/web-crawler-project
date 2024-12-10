f6824345e3429e36e0c7164045c6e4825c6838cc
linor1 <linor.salhov@gmail.com>
2024-12-10 10:58:43 +0200

    url storage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 // index.js
 
 const {getSeedUrls} = require('./services/urlSeeder');
+const { addUrl, getAndRemoveNextUrl, getQueue } = require('./services/urlStorage');
 
 const seedUrls = getSeedUrls();
 
 console.log('Starting Web Crawler...');
+
 console.log('Seed URLs:', seedUrls);
+seedUrls.forEach((url) => addUrl(url));
+
+
+
+
--- src/services/urlStorage.js
+++ src/services/urlStorage.js
@@ -1,2 +1,29 @@
 // urlStorage.js
 
+const urlQueue = new Set();
+
+//add the URL if it is not already in the queue
+function addUrl(url){
+	if (!urlQueue.has(url)){
+		urlQueue.add(url);
+		console.log(`Added URL to queue: ${url}`);
+	}else{
+		console.log(`URL already exists: ${url}`);
+	}
+}
+ 
+//get the next url from the queue and remove it
+function getAndRemoveNextUrl(){
+	const iterator = urlQueue.values();
+	const nextUrl = iterator.next().value;
+	if (nextUrl) {
+		urlQueue.delete(nextUrl);
+		return nextUrl;
+	}
+	return null;
+}
+
+//return all the values
+function getQueue(){
+	return Array.from(urlQueue);
+}
